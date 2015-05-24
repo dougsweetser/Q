8a05ee4ed4d6eb6580a22bfe079fb8cc54334419
Doug Sweetser <sweetser@alum.mit.edu>
2015-05-24 08:36:39 -0400

    Does merge and edit work, like remove labels from images.

---
+++ docs/book_edit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cat book_to_merge.txt | mdmerge -o doing_physics.0.md --book -
+sed 's|../images|images|g' doing_physics.0.md | tee doing_physics.1.md
+perl -ne 'if ($_ =~ /^[\!]/){chomp} print' doing_physics.1.md | tee doing_physics.2.md
+perl -ne '$_ =~ s/^[\!]..*\]/\!\[\]/; print' doing_physics.2.md | tee doing_physics.3.md
+perl -ne 'if ($_ =~ /^[\!]\[[^\]].*[^\)]/){chomp} print' doing_physics.3.md | tee doing_physics.4.md
+perl -ne '$_ =~ s/^[\!]\[[^\]]..*\]/\!\[\]/; print' doing_physics.4.md | tee doing_physics.5.md
+perl -ne 'if ($_ =~ /^[\!]\[[^\]].*[^\)]/){chomp} print' doing_physics.5.md | tee doing_physics.6.md
+perl -ne '$_ =~ s/^[\!]\[[^\]]..*\]/\!\[\]/; print' doing_physics.6.md | tee doing_physics.md
+perl -e '@f = 0..6; for $f (@f){unlink("doing_physics.$f.md");}'
