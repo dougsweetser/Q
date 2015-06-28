ba300017c6c4bfe9ab291ecbc015055ac755972c
Doug Sweetser <sweetser@alum.mit.edu>
2015-06-28 18:09:26 -0400

    Updated.

--- site/book_edit.sh
+++ site/book_edit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pushd . 
+cd ~/Documents/Q/docs
 cat book_to_merge.txt | mdmerge -o doing_physics.0.md --book -
 sed 's|\.\.\/\.\.\/\.\./images|images|g' doing_physics.0.md | tee doing_physics.1.md
 sed 's|\.\.\/\.\./images|images|g' doing_physics.1.md | tee doing_physics.2.md
@@ -12,3 +14,4 @@ perl -ne 'if ($_ =~ /^[\!]\[[^\]].*[^\)]/){chomp} print' doing_physics.8.md | te
 perl -ne '$_ =~ s/^[\!]\[[^\]]..*\]/\!\[\]/; print' doing_physics.9.md | tee doing_physics.10.md
 sed '/./,/^$/!d' doing_physics.10.md | tee doing_physics.md
 perl -e '@f = 0..10; for $f (@f){unlink("doing_physics.$f.md");}'
+popd
