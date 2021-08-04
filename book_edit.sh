7ee5e99600371f5fab71a9843f6405dd5f0cd7c6
Doug Sweetser <sweetser@alum.mit.edu>
2021-08-04 11:22:09 -0500

    Deployed 2434a65 with MkDocs version: 1.1.2

--- book_edit.sh
+++ book_edit.sh
@@ -2,4 +2,13 @@
 pushd . 
 cd ~/workspace/Q/docs
 cat book_to_merge.txt | /home/doug/anaconda3/bin/mdmerge -o doing_physics.md --book -
+sed -i s'/\.\.\/\.\.\/.\.\/images/images/' doing_physics.md
+sed -i s'/\.\.\/\.\.\/images/images/' doing_physics.md
+sed -i s'/\.\.\/images/images/g' doing_physics.md
+sed -i s'/\.\.\/\.\.\/Stuff/Stuff/' doing_physics.md
+sed -i s'/\.\.\/Stuff/Stuff/g' doing_physics.md
+sed -i s'/.Stuff.pdfs.space-times-time_invariance.pdf.//g' doing_physics.md
+sed -i s'/.EM_invariants.md.//g' doing_physics.md
+sed -i s'/.problem_set_1.md.//g' doing_physics.md
+sed -i s'/.*iframe.*//' doing_physics.md
 popd
